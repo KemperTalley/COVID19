ca30922fa18a872dfe475f0a7803f28c5af08b34
Kemper Talley <kemper.talley@gmail.com>
2020-03-13 01:33:56 -0400

    Added a graph for TN
    Moved some lines around for readability and changed scenario to TN

--- EpidemicRisks.py
+++ EpidemicRisks.py
@@ -1,22 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+Python adaptation of Matlab code and concept from Dr. Weirtz at Georgia Tech.
 
-This is a temporary script file.
+https://mobile.twitter.com/joshuasweitz/status/1238173915899293696?fbclid=IwAR2v9x-Ih8o7L45c6BnODB1PbVaNrQVU2w-LWlcPD8DLmNyVu48Ak8Y_viA
+
+Github Repository: https://github.com/jsweitz/covid-19-event-risk-planner/
+
 """
 
 import matplotlib.pyplot as plt
 from matplotlib.offsetbox import AnchoredText
+import matplotlib.ticker as ticker
+#from matplotlib.ticker import ScalarFormatter
 import numpy as np
 
 
+#setup variables for later use
 n=np.logspace(1,5,100)
-pcrit = 0.01/n
-pop = 330*10**6
+#population and location
+#location = 'USA'
+#pop = 330*10**6
+#Tennessee
+location = 'Tennessee'
+pop = 6.77*10**6
+
+#percent risk toleration
 risk_vals = [0.01, 0.02, 0.1, 0.5, 0.9]
 
-plt.close()
 fig, ax = plt.subplots()
+ax.axis([10,100000,10,pop])
+
+
+
+
 
 #draw risk lines
 for i in range(len(risk_vals)):
@@ -24,19 +40,31 @@ for i in range(len(risk_vals)):
     plt.loglog(n,pcrit_risk*pop, label=(str(risk_vals[i]*100) + ' % Chance'))
     
 ax.legend()
-    
-plt.fill_between(n, (1 - (1-risk_vals[0])**(1.0/n))*pop, color='lightgrey')
 
-plt.ylabel('Active circulating infections in the USA, $I$')
-plt.yticks()
+#format the y-axis to be in non-scientific notation
+#for axis in [ax.xaxis, ax.yaxis]:
+#    formatter = ScalarFormatter()
+#    formatter.set_scientific(False)
+#    axis.set_major_formatter(formatter)
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:g}'.format(y)))
+
+
+
+#fill below the 1 percent line    
+plt.fill_between(n, (1 - (1-risk_vals[0])**(1.0/n))*pop, color='lightgrey')
+box_text = '  Less than 1% \n  chance of COVID-19 \n  positive attendee at the event'
+text_box = AnchoredText(box_text, frameon=False, loc=3, pad = 0.5)
+plt.setp(text_box.patch, facecolor = 'white', alpha = 0.5)
+ax.add_artist(text_box)
+
+#print labels
+plt.ylabel('Active circulating infections in ' +location +' , $I$')
+#plt.yticks(labels=[10,100,1000,10000,100000,1000000])
 plt.xlabel('Number of participants at given event')
 plt.title('Chance of a COVID-19 positive participant at an event \n\n Assumes Incidence Homogeneity')
 plt.grid(True)
 
-box_text = 'Less than 1% \n chance of COVID-19 \n positive attendee at the event'
-text_box = AnchoredText(box_text, frameon=False, loc=3, pad = 0.5)
-plt.setp(text_box.patch, facecolor = 'white', alpha = 0.5)
-ax.add_artist(text_box)
+
 
 
 # For these scenarios, use the exact risk assuming homogeneity
